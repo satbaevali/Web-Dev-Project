536c1e3a4ca0a2ff06138f3303fb53631929965b
Alihan <a_satbay@kbtu.kz>
2025-03-31 23:07:06 +0500

    first commit

---
+++ Fronted/Kaspi-store/src/app/components/products-item/products-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-item',
+  imports: [],
+  templateUrl: './products-item.component.html',
+  styleUrl: './products-item.component.css'
+})
+export class ProductsItemComponent {
+
+}
